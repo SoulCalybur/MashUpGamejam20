deb164dfc9932b9bde9bb18bb8645cd0129615a4
JeremyB <baltatj@fh-trier.de>
2020-06-06 23:37:30 +0200

    rendering a crosshair around the player with max distance

--- Assets/Scripts/PlayerAim.cs
+++ Assets/Scripts/PlayerAim.cs
@@ -1,18 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerAim : MonoBehaviour
-{
+public class PlayerAim : MonoBehaviour {
     private Vector2 mousePos = new Vector2();
-    private Vector2 ownScreenPos = new Vector2();
+    private Vector2 playerScreenPos = new Vector2();
+    private Vector2 lookDir = new Vector2();
 
-    public void OnLook(InputValue input)
-    {
+    const float MAX_DIST = 50; //pixel units
+
+    [SerializeField]
+    RectTransform crosshair;
+
+    private void Awake() {
+        crosshair = crosshair.GetComponent<RectTransform>();
+    }
+
+    public void OnLook(InputValue input) {
         mousePos = input.Get<Vector2>();
-        //Debug.Log(mousePos);
-        Vector3 temp = Camera.main.WorldToScreenPoint(transform.position);
-        Vector2 screenPos = new Vector2(temp.x, temp.y);
+    }
 
-        Debug.Log("ScreenPos:" + screenPos + " --- " + mousePos + " --- " + (mousePos - screenPos));
+    void FixedUpdate() {
+        Vector3 temp = Camera.main.WorldToScreenPoint(transform.position);
+        playerScreenPos = new Vector2(temp.x, temp.y);
+
+        lookDir = (mousePos - playerScreenPos);
+        float distance = lookDir.magnitude;
+
+        if (distance > MAX_DIST) {
+            crosshair.position = playerScreenPos + lookDir.normalized * MAX_DIST;
+        } else {
+            crosshair.position = new Vector3(mousePos.x, mousePos.y, 0);
+        }
     }
 }
