401e2089bb3b898b1567868834b7b9852167032e
JeremyB <baltatj@fh-trier.de>
2020-06-08 00:48:17 +0200

    balloons are stored in player temporarily; Pumpup balloons with LEFT_MOUSE click after spawning with E

--- Assets/Scripts/Balloon.cs
+++ Assets/Scripts/Balloon.cs
@@ -4,12 +4,42 @@ using UnityEngine;
 
 public class Balloon : MonoBehaviour {
 
+    [SerializeField]
     private int curAirVolume = 10;
     private const int MAX_AIR_VOLUME = 100;
 
+    [SerializeField]
+    private Transform balloonSprite;
 
-    void Blowup(int amount) {
-
+    public bool PumpUp(int amount) {
+        Debug.Assert(amount > 0, "Negative balloon blowup-amount!");
+        curAirVolume += amount;
+        balloonSprite.localScale = new Vector3(balloonSprite.localScale.x * 1.1f, balloonSprite.localScale.y * 1.1f, 0);
+        balloonSprite.GetComponent<SpriteRenderer>().color = new Color((float)curAirVolume / (float)MAX_AIR_VOLUME, 0,0);
+        if(curAirVolume >= MAX_AIR_VOLUME) {
+            curAirVolume = MAX_AIR_VOLUME;
+            Explode();
+            Despawn();
+            return true;
+        }
+        return false;
     }
-    
+
+    public void PumpOut(int amount) {
+        Debug.Assert(amount > 0, "Negative balloon blowup-amount!");
+        curAirVolume -= amount;
+        if (curAirVolume <= MAX_AIR_VOLUME) {
+            curAirVolume = 0;
+            Despawn();
+        }
+    }
+
+    private void Explode() {
+        Debug.Log("Balloon exploded!");
+    }
+    private void Despawn() {
+        Debug.Log("Despawn Balloon");
+        Destroy(this.gameObject);
+    }
+
 }
--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -11,6 +11,8 @@ public class PlayerInteraction : MonoBehaviour {
     [SerializeField]
     private int balloonCapacity = 3;
 
+    private List<Transform> balloons = new List<Transform>();
+
     public void OnInteract(InputValue input) {
         if (input.isPressed && balloonCapacity > 0) {
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, LayerMask.GetMask("Ground"));
@@ -21,11 +23,31 @@ public class PlayerInteraction : MonoBehaviour {
             }
         }
     }
+
+    public void OnFire(InputValue input) {
+        if (input.isPressed) {
+            PumpUpBalloon();
+        }
+    }
     private void SpawnBalloon(Vector2 origin) {
         Debug.Assert(balloonPrefab != null, "No prefab chosen in Inspector");
         Debug.Assert(balloonCapacity > 0, "Not enough balloons left");
-        Instantiate(balloonPrefab, origin, Quaternion.identity, transform.parent);
+        balloons.Add(Instantiate(balloonPrefab, origin, Quaternion.identity, transform.parent));
         balloonCapacity--;
         if(balloonCapacity < 0) balloonCapacity = 0;
     }
+
+    private void PumpUpBalloon() {
+        Debug.Log("Pumping up all balloons");
+        //foreach (Transform balloon in balloons) { // ERROR HERE
+        //    int indexOfB = balloons.IndexOf(balloon);
+        //    bool exploded = balloon.GetComponent<Balloon>().PumpUp(10);
+        //}
+
+        for (int i = 0; i < balloons.Count; i++) {
+            bool exploded = balloons[i].GetComponent<Balloon>().PumpUp(10);
+            if (exploded) balloons.RemoveAt(i--); // i-- because of removing item from currently iterating list
+        }
+    }
+
 }
