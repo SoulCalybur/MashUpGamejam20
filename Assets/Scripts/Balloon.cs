18628e5f9d6502f7c16ca5231d3601b99471ff32
JeremyB <baltatj@fh-trier.de>
2020-06-07 23:47:54 +0200

    Added joints to balloon to make it swing around

---
+++ Assets/Scripts/Balloon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour {
+
+    private int curAirVolume = 10;
+    private const int MAX_AIR_VOLUME = 100;
+
+
+    void Blowup(int amount) {
+
+    }
+    
+}
